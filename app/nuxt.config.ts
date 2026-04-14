34397e8eba7f4710a9d6dafa73bdff889104537b
Tolibjon <tolibjonfayz@gmail.com>
2026-04-14 16:31:30 +0500

    Vercel error

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
 
   css: ["~/assets/scss/main.scss"],
 
+  nitro: {
+    preset: "vercel",
+  },
+
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:3003/api",
